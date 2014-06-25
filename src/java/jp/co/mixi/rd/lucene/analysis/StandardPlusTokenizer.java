678ac9a24bb74f477f907addd6a027129ddcd799
HARUYAMA Seigo <haruyama@unixuser.org>
2014-06-26 00:36:01 +0900

    Version 4.9.0

--- src/java/jp/co/mixi/rd/lucene/analysis/StandardPlusTokenizer.java
+++ src/java/jp/co/mixi/rd/lucene/analysis/StandardPlusTokenizer.java
@@ -26,6 +26,8 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
 import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
+import org.apache.lucene.util.AttributeFactory;
+import org.apache.lucene.util.AttributeSource;
 import org.apache.lucene.util.Version;
 
 /** A grammar-based tokenizer constructed with JFlex.
@@ -125,7 +127,7 @@ public final class StandardPlusTokenizer extends Tokenizer {
   }
 
   /**
-   * Creates a new StandardPlusTokenizer with a given {@link org.apache.lucene.util.AttributeSource.AttributeFactory}
+   * Creates a new StandardPlusTokenizer with a given {@link org.apache.lucene.util.AttributeFactory}
    */
   public StandardPlusTokenizer(Version matchVersion, AttributeFactory factory, Reader input) {
     super(factory, input);
